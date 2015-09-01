0b0d53d4fc292d4663f02d4689b125d6c8d2dc6e
Harry Ng <harryworld@gmail.com>
2015-09-01 12:51:58 +0800

    Add StubRouterContext for testing routes

--- app/components/layouts/__tests__/App-test.js
+++ app/components/layouts/__tests__/App-test.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import TestUtils from 'react/lib/ReactTestUtils';
+
 import App from '../App';
+import StubRouterContext  from '../../../support/stub_router_context';
 
 describe('App', () => {
-  it('renders');
+  it('renders', (done) => {
+    let Subject = StubRouterContext(App)
+    let element = TestUtils.renderIntoDocument(<Subject />);
+
+    expect(element).toBeTruthy();
+    done();
+  });
 });
